2a4651a41df3923e9c5c9e4677c866a0fdfb8ebb
Daniel Spiessberger <daniel.spiessberger@gmail.com>
2026-06-09 19:09:38 +0200

    fix timer period

--- Src/main.c
+++ Src/main.c
@@ -259,7 +259,7 @@ static void MX_TIM10_Init(void)
   htim10.Instance = TIM10;
   htim10.Init.Prescaler = 1;
   htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim10.Init.Period = 42000;
+  htim10.Init.Period = 41999;
   htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
